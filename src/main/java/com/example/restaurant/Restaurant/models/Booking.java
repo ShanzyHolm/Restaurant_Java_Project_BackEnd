1cd501bb96d08859fd7b6d4921904bb5fdf8dc2d
Craig Bowditch <craigbowditch@hotmail.com>
2018-10-18 15:15:44 +0100

    adds customer class

---
+++ src/main/java/com/example/restaurant/Restaurant/models/Booking.java
@@ -0,0 +1,14 @@
+package com.example.restaurant.Restaurant.models;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+public class Booking {
+
+
+
+
+}
---
+++ src/main/java/com/example/restaurant/Restaurant/models/Table.java
@@ -0,0 +1,4 @@
+package com.example.restaurant.Restaurant.models;
+
+public class Table {
+}
