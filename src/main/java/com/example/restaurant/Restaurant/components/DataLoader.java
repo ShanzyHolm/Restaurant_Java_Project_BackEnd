8d16e7b6c1b1db421d08641082f342f217cdf184
Craig Bowditch <craigbowditch@hotmail.com>
2018-10-19 10:50:13 +0100

    adds more seed data

--- src/main/java/com/example/restaurant/Restaurant/components/DataLoader.java
+++ src/main/java/com/example/restaurant/Restaurant/components/DataLoader.java
@@ -43,13 +43,34 @@ public class DataLoader implements ApplicationRunner {
            e.printStackTrace();
        }
 
-       Customer customer = new Customer("Jobby", "123");
-       customerRepository.save(customer);
+       Customer customer1 = new Customer("Customer1", "123");
+       customerRepository.save(customer1);
 
-       Table table = new Table("jobbytable", 4);
-       tableRepository.save(table);
+       Customer customer2 = new Customer("Customer2", "123");
+       customerRepository.save(customer2);
 
-       Booking booking = new Booking(date, customer, table);
-       bookingRepository.save(booking);
+       Customer customer3 = new Customer("Customer3", "123");
+       customerRepository.save(customer3);
+
+       Table table1 = new Table("Table1", 4);
+       tableRepository.save(table1);
+
+       Table table2 = new Table("Table2", 4);
+       tableRepository.save(table2);
+
+       Table table3 = new Table("Table3", 4);
+       tableRepository.save(table3);
+
+       Booking booking1 = new Booking(date, customer1, table1);
+       bookingRepository.save(booking1);
+
+       Booking booking2 = new Booking(date, customer1, table2);
+       bookingRepository.save(booking2);
+
+       Booking booking3 = new Booking(date, customer2, table3);
+       bookingRepository.save(booking3);
+
+       Booking booking4 = new Booking(date, customer3, table2);
+       bookingRepository.save(booking4);
     }
 }
