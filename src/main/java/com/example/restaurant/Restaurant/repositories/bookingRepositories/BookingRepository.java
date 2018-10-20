ecd9457d95668e8f187a0bcf0bee566c7f7751e0
fddata <fraserjdouglas@gmail.com>
2018-10-20 12:09:26 +0100

    adds to bookingRespositories

--- src/main/java/com/example/restaurant/Restaurant/repositories/bookingRepositories/BookingRepository.java
+++ src/main/java/com/example/restaurant/Restaurant/repositories/bookingRepositories/BookingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 @RepositoryRestResource(excerptProjection = EmbedCustomer.class)
-public interface BookingRepository extends JpaRepository<Booking, Long> {
+public interface BookingRepository extends JpaRepository<Booking, Long>, BookingRepositoryCustom {
 }
